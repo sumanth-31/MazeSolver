620c9da2c65c35ac94ccdf2d47679ae753bee5bf
sumanth-31 <sumanthchowdary31@gmail.com>
2024-12-03 21:56:37 -0500

    Saving WIP

---
+++ src/main/java/com/uga/mazesolvingagent/InnerMazeSolvingAgentApplication.java
@@ -0,0 +1,17 @@
+package com.uga.mazesolvingagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * Entry point for the server. Defines the main method which boots up the server for the back-end.
+ */
+
+@SpringBootApplication
+public class InnerMazeSolvingAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnerMazeSolvingAgentApplication.class, args);
+	}
+
+}
